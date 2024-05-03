f33b5c617bf1289c49c79cba4d8499cf308cd3fe
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-03 10:35:36 +0700

    if the node to be deleted is first node

--- DoublyLinkedList/DoublyLinkedList.cpp
+++ DoublyLinkedList/DoublyLinkedList.cpp
@@ -96,4 +96,12 @@ void deleteNode()
 		cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
 		return;
 	}
+	if (current == START)
+	{
+		START = START->next;
+		if (START != NULL)
+		{
+			START->prev = NULL;
+		}
+	}
 }
