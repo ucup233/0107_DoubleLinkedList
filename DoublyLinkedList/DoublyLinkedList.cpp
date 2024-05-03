6dedede6c69393b610aa1d85f9254f8de9bb212f
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-03 10:33:03 +0700

    prosedur delete, rollNo sebagai data yang akan di delete

--- DoublyLinkedList/DoublyLinkedList.cpp
+++ DoublyLinkedList/DoublyLinkedList.cpp
@@ -67,3 +67,12 @@ bool search(int rollNo, Node** previous, Node** current)
 	}
 	return (*current != NULL);
 }
+
+void deleteNode()
+{
+	Node* previous, * current;
+	int rollNo;
+
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo;
+}
